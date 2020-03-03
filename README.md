02eb465032b9f6a3b342449d27589eee902d9481
Ling Li <ling.li@felcana.com>
2020-03-03 17:09:44 +0000

    ensure script works with BSS export

--- README.md
+++ README.md
@@ -3,10 +3,19 @@ A collection of scripts/tools to help export from Bootstrap Studio(BSS) to djang
 
 Prerequistes
 ------------
-Install beautifulSoup 4, see: https://www.crummy.com/software/BeautifulSoup/bs4/doc/
+Install beautifulSoup 4, see: https://www.crummy.com/software/BeautifulSoup/bs4/doc/ ideally in a virtualenv, 
+eg:
+```
+python -m venv ~/venv/djbss
+source ~/venv/djbss/bin/activate
+python -m pip install pipenv
+pipenv install
+```
 
 or in Ubuntu:
+```
 sudo apt-get install python3-bs4
+```
 
 To run:
 
@@ -17,6 +26,12 @@ Currently supports following attributes for django conversion:
 dj-for   => will create a for loop
 dj-refs  => will create ref to variable
 dj-if    => will create if block
+dj-block => will create a block
+
+In bootstrap Studio, you can include the script `export_settings.sh` in the advance option in the export settings.
+So your html files will automatically be converted into django template format.
+If this is not working, you will need to copy the `export.py` script to the location of your BSS app image.
+Also you need to ensure that you have BeautifulSoup installed correctly.
 
 
 TODO:
--- export.py
+++ export.py
@@ -1,38 +1,52 @@
 import sys
 from bs4 import BeautifulSoup
 
+# convert a file from Bootstrap Studio to Django Template
+
 infile = sys.argv[-1]
 
-with open( infile ) as fp:
+with open(infile, "r") as fp:
     soup = BeautifulSoup(fp, "lxml")
-    # handle for
-    for div in soup.find_all(attrs={"dj-for": True}):
-        if div:
-            forline = "{% for " + div.get('dj-for') + " %}"
-            div.insert_before(forline)
-            if 'dj-for' in div.attrs:
-                del div.attrs['dj-for']
-                div.insert_after('{% endfor %}')
 
-    # handle refs
-    for ref in soup.find_all(attrs={"dj-ref": True}):
+# handle for
+for div in soup.find_all(attrs={"dj-for": True}):
+    if div:
+        forline = "{% for " + div.get('dj-for') + " %}"
+        div.insert_before(forline)
+        if 'dj-for' in div.attrs:
+            del div.attrs['dj-for']
+            div.insert_after('{% endfor %}')
+
+# handle refs
+for ref in soup.find_all(attrs={"dj-ref": True}):
+    if ref:
         if 'dj-ref' in ref.attrs:
             refattr = ref.get('dj-ref')
-            ref.string.replace_with('{{ ' + refattr + ' }}')
+            if ref.string:
+                ref.string.replace_with('{{ ' + refattr + ' }}')
             del ref.attrs['dj-ref']
 
-    # handle if
-    # eg: <div dj-if="form.errors" >  will get translated to: 
-    # {% if form.errors %}
-    # ...
-    # {% endif %}
-    for ifs in soup.find_all(attrs={"dj-if": True}):
-        if ifs:
-            ifline = "{% if " + ifs.get("dj-if") + " %}"
-            ifs.insert_before(ifline) 
-            if "dj-if" in ifs.attrs: 
-                del ifs.attrs["dj-if"]
-                ifs.insert_after("{% endif %}")
+# handle if
+# eg: <div dj-if="form.errors" >  will get translated to: 
+# {% if form.errors %}
+# ...
+# {% endif %}
+for ifs in soup.find_all(attrs={"dj-if": True}):
+    if ifs:
+        ifline = "{% if " + ifs.get("dj-if") + " %}"
+        ifs.insert_before(ifline)
+        if "dj-if" in ifs.attrs:
+            del ifs.attrs["dj-if"]
+            ifs.insert_after("{% endif %}")
 
+# handle block
+for div in soup.find_all(attrs={"dj-block": True}):
+    if div:
+        blockline = "{% block " + div.get('dj-block') + " %}"
+        div.insert_before(blockline)
+        if 'dj-block' in div.attrs:
+            del div.attrs['dj-block']
+            div.insert_after('{% endblock %}')
 
-    print (soup.prettify())
+with open(infile, "w") as outfp:
+    outfp.write(soup.prettify())
