3b4eafeeb6ea0a063824f4b5547a5186c2e9ad7a
AbcSxyZ <rossi.sim@outlook.com>
2020-08-06 22:09:53 +0200

    Avoid having script in bss export directory

--- export_settings.sh
+++ export_settings.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
 
 # this will read all html files and replace dj-* attributes with django code
-# YOU MAY NEED TO copy the python export.py script to the same folder as where BSS app image is located
 
 logfile="/tmp/bss_export.log"
-cwd=`pwd`
+export_script=$(realpath lol.py)
+
+cd $1
 
 # if you have a venv setup, replace the following
 source ~/venv/django-bss-tools/bin/activate
 
 echo "run at `date`" >> $logfile
 echo "`which python3`" >> $logfile
-echo $cwd >> $logfile
 echo $1 >> $logfile
-pushd $1
 for f in *.html
 do
     echo python3 $cwd/export.py $f >> $logfile 
-    python3 $cwd/export.py $f 
+    python3 $export_script $f 
 done
-popd
-
